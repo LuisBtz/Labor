72515059737d51bea1025c43daf921eaaac12d98
Luis B <luisbttf@gmail.com>
2021-04-20 09:17:02 -0500

    query

--- gatsby/src/templates/Artist.js
+++ gatsby/src/templates/Artist.js
@@ -45,8 +45,105 @@ export const query = graphql`
                 _key
                 _type
                 inlineFullImage {
+                caption {
+                    es
+                }
+                alternativeText
                 asset {
                     gatsbyImageData(layout: FULL_WIDTH, outputPixelDensities: 1.5)
+                }
+                }
+            }
+            ... on SanityBodyText {
+                _key
+                _type
+                es {
+                headline
+                paragraph
+                }
+            }
+            ... on SanityCarousel {
+                _key
+                _type
+                addImage {
+                alternativeText
+                caption {
+                    es
+                }
+                asset {
+                    gatsbyImageData(layout: FULL_WIDTH, outputPixelDensities: 1.5)
+                }
+                }
+            }
+            ... on SanityColophon {
+                _key
+                _type
+                colophoneText {
+                es
+                }
+                technicalInfo {
+                es
+                }
+            }
+            ... on SanityImageTwoColumns {
+                _key
+                _type
+                imageOne {
+                alternativeText
+                caption {
+                    es
+                }
+                asset {
+                    gatsbyImageData(layout: FULL_WIDTH)
+                }
+                }
+                imageTwo {
+                alternativeText
+                caption {
+                    es
+                }
+                asset {
+                    gatsbyImageData(layout: FULL_WIDTH, outputPixelDensities: 1.5)
+                }
+                }
+            }
+            ... on SanityMainHeadline {
+                _key
+                _type
+                buttons {
+                es {
+                    social
+                    resume
+                    dossier
+                    agenda
+                }
+                }
+                textField {
+                es
+                }
+            }
+            ... on SanityQuote {
+                _key
+                _type
+                quoteText {
+                es
+                }
+            }
+            ... on SanityVideo {
+                _key
+                _type
+                titleCode
+            }
+            ... on SanityZoom {
+                _key
+                _type
+                imageZoom {
+                caption {
+                    es
+                }
+                alternativeText
+                asset {
+                    gatsbyImageData(layout: FULL_WIDTH)
                 }
                 }
             }
