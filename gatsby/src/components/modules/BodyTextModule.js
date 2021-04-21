0f17439e9c48f6c68a5203788a5e1c1279c4c4fd
Luis B <luisbttf@gmail.com>
2021-04-21 16:53:06 -0500

    BodyTextModule

--- gatsby/src/components/modules/BodyTextModule.js
+++ gatsby/src/components/modules/BodyTextModule.js
@@ -1,23 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import BlockContent from '@sanity/block-content-to-react';
 
-const BodyTextModule = () => {
+
+const BodyTextModule = ( { data } ) => {
+
+
   return (
     <BodyTextWrapper>
       <h5 className="bold">
-        At solmen va esser necessi far uniform grammatica, pronunciation e plu
-        sommun paroles
+
+      {data.es && data.es.headline && 
+          (data.es.headline ? data.es.headline : '')
+        }
+
       </h5>
       <p>
-        An index and its settings (Un índice y sus entornos) se presenta en
-        LABOR, como parte de una investigación en curso basada en la colección
-        Proctor Stafford, un conjunto de 235 figuras y vasijas de cerámica (ca.
-        200 AEC – 500 EC) de las costas del Pacífico de México, de las ciudades
-        de Colima, Nayarit y Jalisco, que hoy en día forman parte de la
-        colección de Los Angeles County Museum Museum of Art (LACMA). El
-        proyecto se divide en los nombres de los tres estados y aborda las
-        estipulaciones sobre la nomenclatura de la colección, su procedencia y
-        las historias que se construyen a partir de la información disponible.
+      {data.es && data.es._rawParagraph && 
+          (data.es._rawParagraph ? 
+            <BlockContent
+            blocks={data.es._rawParagraph}
+        />
+         : '')
+        }
       </p>
     </BodyTextWrapper>
   );
