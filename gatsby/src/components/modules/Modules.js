bd75abbe5167c407a90ff4ea2a2c177da6b1674e
Khuzaima <Khuzaimayaseen@gmail.com>
2021-04-21 22:25:42 +0500

    Apply logics in Module.js to show content dynamically..

--- gatsby/src/components/modules/Modules.js
+++ gatsby/src/components/modules/Modules.js
@@ -1,35 +1,50 @@
-import React from 'react'
-import styled from 'styled-components';
-import BodyTextModule from './BodyTextModule';
-import CarouselModule from './CarouselModule';
-import ColophonModule from './ColophonModule';
-import FullImageModule from './FullImageModule';
-import MainHeadlineModule from './MainHeadlineModule';
-import QuoteModule from './QuoteModule';
-import TwoColumnsImageModule from './TwoColumnsImageModule';
-import VideoModule from './VideoModule';
-import ZoomModule from './ZoomModule';
+import React from "react";
+import styled from "styled-components";
+import BodyText from "./BodyTextModule";
+import Carousel from "./CarouselModule";
+import Colophon from "./ColophonModule";
+import FullImage from "./FullImageModule";
+import MainHeadline from "./MainHeadlineModule";
+import Quote from "./QuoteModule";
+import TwoColumnsImage from "./TwoColumnsImageModule";
+import Video from "./VideoModule";
+import Zoom from "./ZoomModule";
 
-const Modules = ({ editorialModules }) => {
+const modulesArr = [
+  { name: "bodyText", comp: BodyText },
+  { name: "carousel", comp: Carousel },
+  { name: "colophon", comp: Colophon },
+  { name: "fullImage", comp: FullImage },
+  { name: "mainHeadline", comp: MainHeadline },
+  { name: "quote", comp: Quote },
+  { name: "imageTwoColumns", comp: TwoColumnsImage },
+  { name: "video", comp: Video },
+  { name: "zoom", comp: Zoom },
+];
 
-  console.log(editorialModules)
+const Modules = ({ editorialModule }) => {
+  console.log(editorialModule);
+  let final_arr = [];
+
+  editorialModule.forEach((edMod) => {
+    modulesArr.forEach((mMod) => {
+      if (edMod["_type"] === mMod.name) {
+        final_arr.push(mMod);
+      }
+    });
+  });
+
+  console.log("Final = ", final_arr);
 
   return (
-    <ModulesWrapper >
-      <MainHeadlineModule />
-      <BodyTextModule />
-      <FullImageModule />
-      <QuoteModule />
-      <TwoColumnsImageModule />
-      <ColophonModule />
-      <CarouselModule />
-      <VideoModule />
-      <ZoomModule />
+    <ModulesWrapper>
+      {final_arr.map((Module, i) => (
+        <Module.comp key={i} />
+      ))}
     </ModulesWrapper>
+  );
+};
 
-  )
-}
+const ModulesWrapper = styled.div``;
 
-const ModulesWrapper = styled.div``
-
-export default Modules
+export default Modules;
