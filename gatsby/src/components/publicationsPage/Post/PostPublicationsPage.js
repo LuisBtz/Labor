effc6057bc0bf28d2ada0cc01897320e99332735
Fernanda Ruiz <ruizfernanda2@gmail.com>
2021-04-20 22:47:57 -0500

    css styling

--- gatsby/src/components/publicationsPage/Post/PostPublicationsPage.js
+++ gatsby/src/components/publicationsPage/Post/PostPublicationsPage.js
@@ -37,9 +37,17 @@ const PostExpositionWrapper = styled(Link)`
   padding-right: 2vw;
   display: flex;
 
+  @media screen and (max-width: 1280px) {
+    min-width: 60vw;
+  }
+
+  @media screen and (max-width: 1040px) {
+    min-width: 75vw;
+  }
+  
   /* Mobile */
   @media screen and (max-width: 768px) {
-    min-width: 75vw;
+    min-width: 77vw;
     padding-right: 0;
     flex-direction: column;
   }
@@ -58,7 +66,7 @@ const Text = styled.div`
   box-sizing: border-box;
   width: 50%;
   margin-top: 1rem;
-  margin-bottom: 5rem;
+  margin-bottom: 2rem;
   padding: .5rem 1rem;
 
   h5 {
@@ -73,8 +81,8 @@ const Text = styled.div`
 `
 
 const Abstract = styled.p`
-  margin-top: 25px;
-  font-size: 16px;
+  margin-top: 1.5rem;
+  font-size: var(--paragraph);
   text-indent: 2rem;
 `
 
