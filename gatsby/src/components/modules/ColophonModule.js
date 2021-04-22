dc20e3ec94c688e2a688c306e26383c580866a25
Luis B <luisbttf@gmail.com>
2021-04-21 19:22:43 -0500

    Colophone Module

--- gatsby/src/components/modules/ColophonModule.js
+++ gatsby/src/components/modules/ColophonModule.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const ColophonModule = () => {
+const ColophonModule = ( { data } ) => {
+
 
   return (
 
     <ColophonWrapper>
-      <Colophon>
-        <h4 className="small bold"><span className="arrow">↓</span>COLOPHON</h4>
-        <p className="small">September 2015 Edited by Anne Kaestle and Dan Schürch Published by Anne Kaestle and Dan Schürch Language: German Book concept and design: Ludovic Balland Typography Cabinet with Andreas Hänggi</p>
-      </Colophon>
-      <Info>
-        <h4 className="small bold"><span className="arrow">↓</span>TECHNICAL INFORMATION</h4>
-        <p className="small">Softcover, 160 × 240 mm, 164 pages Typeface: Next by Ludovic Balland Print: CPI, France</p>
-      </Info>
+    {data.colophoneText && data.colophoneText.es &&
+          (data.colophoneText.es ? <Colophon>
+            <h4 className="small bold"><span className="arrow">↓</span>COLOPHON</h4>
+            <p className="small">{data.colophoneText.es}</p>
+          </Colophon> : '')
+        }
+
+{data.technicalInfo && data.technicalInfo.es &&
+          (data.technicalInfo.es ? 
+          <Info>
+            <h4 className="small bold"><span className="arrow">↓</span>TECHNICAL INFORMATION</h4>
+            <p className="small">{data.technicalInfo.es}</p>
+          </Info> : '')
+        }
+      
+      
     </ColophonWrapper>
 
   )
