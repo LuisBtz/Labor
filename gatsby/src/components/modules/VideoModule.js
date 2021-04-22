65117ea26c1607c51966e824ad83a9cec06e20b0
Luis B <luisbttf@gmail.com>
2021-04-21 20:43:46 -0500

    Video Module

--- gatsby/src/components/modules/VideoModule.js
+++ gatsby/src/components/modules/VideoModule.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const VideoModule = () => {
+const VideoModule = ( { data } ) => {
 
   return (
 
-    <VideoWrapper>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/r79agtknbJk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </VideoWrapper>
+      <VideoWrapper dangerouslySetInnerHTML={{
+                __html: data.titleCode
+      }}/>
 
   )
 }
