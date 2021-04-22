83c9773fcbee0bafdb0b515bc65c9e9b0f302ae5
Luis B <luisbttf@gmail.com>
2021-04-21 19:50:15 -0500

    Full Image Module

--- gatsby/src/components/modules/FullImageModule.js
+++ gatsby/src/components/modules/FullImageModule.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import styled from 'styled-components';
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const FullImageModule = () => {
+
+const FullImageModule = ( { data } ) => {
+
+  const imageFull = getImage(data.inlineFullImage.asset);  
+
 
   return (
 
     <FullImageWrapper>
-      <StaticImage
-        src="https://images.unsplash.com/photo-1497369542358-287e35b2ce4e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80"
-        alt="A dinosaur"
-        placeholder="blurred"
-        layout="fullWidth"
-      />
-      <p className="detail">caption</p>
+
+        {data.inlineFullImage &&
+          (data.inlineFullImage.alternativeText ? 
+            <GatsbyImage
+              image={imageFull}
+              alt={data.inlineFullImage.alternativeText}
+          /> 
+              : <GatsbyImage
+              image={imageFull}
+              alt='Labor Image'
+          />  )
+        }
+
+        {data.inlineFullImage && data.inlineFullImage.caption && data.inlineFullImage.caption.es &&
+          (data.inlineFullImage.caption.es ? 
+            <p className="detail">{data.inlineFullImage.caption.es}</p>
+              : ''  )
+        }
+      
     </FullImageWrapper>
 
   )
