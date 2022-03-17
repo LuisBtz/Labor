b2bad9ff7a42e794bb57e3092f4063d0d3aa6856
Luis B <luisbttf@gmail.com>
2022-03-16 23:11:38 -0600

    Newsletter

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -34,33 +34,7 @@ module.exports = {
               timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
             },
           },
-          // {
-          //   resolve: `gatsby-source-datocms`,
-          //   options: {
-          //     // You can find your read-only API token under the Settings > API tokens
-          //     // section of your administrative area. Make sure to grant both CDA and CMA permissions.
-          //     apiToken: `9df1785fa4475174d417aedd30ea91`,
-        
-          //     // The project environment to read from. Defaults to the primary environment:
-          //     environment: `main`,
-        
-          //     // If you are working on development/staging environment, you might want to
-          //     // preview the latest version of records instead of the published one:
-          //     previewMode: false,
-        
-          //     // Disable automatic reloading of content when some change occurs on DatoCMS:
-          //     disableLiveReload: false,
-        
-          //     // Custom API base URL (most don't need this)
-          //     // apiUrl: 'https://site-api.datocms.com',
-        
-          //     // Setup locale fallbacks
-          //     // In this example, if some field value is missing in Italian, fall back to English
-          //     localeFallbacks: {
-          //       en: ['es'],
-          //     },
-          //   },
-          // },
+          // Endpoint Newsletter
           {
             resolve: `gatsby-source-sanity`,
             options: {
